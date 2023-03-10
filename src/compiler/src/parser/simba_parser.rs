7363946b7fa76c9cb39620ce12632854121b11b1
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-03-10 20:52:48 +0100

    Template for parser and ast.

--- src/compiler/src/parser/simba_parser.rs
+++ src/compiler/src/parser/simba_parser.rs
@@ -3,3 +3,17 @@
 pub struct SimbaParser {
 
 }
+
+
+pub trait LanguageParser {
+    fn new(tab_size: u8) -> SimbaParser;
+}
+
+
+impl LanguageParser for SimbaParser {
+    fn new(tab_size: u8) -> SimbaParser {
+        SimbaParser {
+
+        }
+    }
+}
